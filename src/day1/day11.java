a021e6c549b9a2e5ce4109f48f66730d8caa31fe
DESKTOP-HJQTCH9\admin <769256719@qq.com>
2019-08-24 22:08:58 +0800

    常量

--- src/day1/day11.java
+++ src/day1/day11.java
@@ -1,10 +1,17 @@
 package day1;
 
 /**
- *  备注 测试git账号
+ * 备注 测试git账号
+ */
+/**
+ 傻bb
+ 常量是不能改变的数据
+
  */
 public class day11 {
     public static void main(String[] args) {
         System.out.println("丢你吗你麻痹");
+        System.out.println("丢你吗嘿");
+        System.out.println("盘他");
     }
 }
