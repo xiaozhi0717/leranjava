9de6486eb98476d56674735a5f604913a8886bdb
DESKTOP-HJQTCH9\admin <769256719@qq.com>
2019-08-26 22:08:23 +0800

    变量

--- src/day1/day11.java
+++ src/day1/day11.java
@@ -3,10 +3,26 @@ package day1;
 /**
  *  备注 测试git账号
  *  常量是不能改变的数值
+ *  变量就如同数字中的未知数
+ *  格式就是数据类型 变量名 = 初始化值;
  */
 public class day11 {
     public static void main(String[] args) {
         System.out.println("丢你吗你麻痹");
         System.out.println("傻嗨");
+        byte b=3;
+        short s=4;
+        int i= 7;
+        float z=2.3f;
+        char c=1;
+        boolean bo=true;
     }
 }
+/**
+ * 数据类型--基本数据类型和引用数据类型
+ * 基本数据类型--数值型 字符型(char) 布尔型(boolean)
+ * 数值型--整数类型(byte short int long) 浮点类型(flat double)
+ * 引用数据类型--类(class) 接口(interface) 数据([])
+ * 整数默认int 浮点默认double 浮点就是小数
+ * long 对于过大的数,加l float 小数后加f可以表示单精度存储
+ */
